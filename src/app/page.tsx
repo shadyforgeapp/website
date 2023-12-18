13c82a16db8ae60390489be3dd914e66097102b2
Mani Kandan <mani@shadyforge.com>
2023-12-18 15:56:28 -0700

    feat: scrollarea

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area'
 
 const projects = [
   {
@@ -22,59 +23,82 @@ const projects = [
 
 export default async function Home() {
   return (
-    <main className='font-editorialul mx-3 min-h-screen max-w-full items-center px-6 pb-16'>
+    <main className='font-editorialul min-h-screen max-w-full items-center px-6 pb-16'>
       <div className='rounded-b-sm border-b-[1.5px] border-b-stone-400 border-opacity-20 py-4'>
         <h1 className=' text-xl text-stone-600'>shadyforge</h1>
         <h2 className='tracking-tight text-stone-400 '>by mani & friends</h2>
       </div>
       <h2 className='py-4 text-lg'>products</h2>
-      <div className='flex gap-6'>
-        <div className='w-64'>
-          <div className='flex h-64 items-center justify-center rounded-2xl border-[0.25px] border-stone-400 border-opacity-40 bg-stone-50'>
-            <Link href={'https://plzreply.com'}>
-              <h2 className='font-fonty glowing-text whitespace-nowrap text-3xl text-[#00b0c8]'>
-                Plz Reply
-              </h2>
-            </Link>
-          </div>
-          <p className='py-[6px] text-xs text-stone-400'>
-            2023 &nbsp;with{' '}
-            <Link className='underline' href={'https://twitter.com/eabnelson'}>
-              erik
-            </Link>
-          </p>
-        </div>
-        {projects
-          .slice()
-          .reverse()
-          .map((project) => (
-            <div key={project.name} className='w-64'>
-              <div className='flex h-64 items-center justify-center rounded-2xl border-[0.25px] border-stone-400 border-opacity-40 bg-stone-50'>
-                <Link href={project.url}>
-                  <Image
-                    className='w-24'
-                    alt={`${project.name} logo`}
-                    src={project.logo}
-                    width={96}
-                    height={24}
-                  />
+      <ScrollArea className='whitespace-nowrap'>
+        <div className='flex w-max space-x-6'>
+          <div>
+            <div className='flex h-64 w-64 items-center justify-center rounded-2xl border-[0.25px] border-stone-400 border-opacity-40 bg-stone-50'>
+              <div className='rounded-md'>
+                <Link href={'https://plzreply.com'}>
+                  <h2 className='font-fonty glowing-text whitespace-nowrap text-3xl text-[#00b0c8]'>
+                    Plz Reply
+                  </h2>
                 </Link>
               </div>
-              <p className='py-[6px] text-xs text-stone-400'>{project.year}</p>
             </div>
-          ))}
-      </div>
-      <h2 className='py-4 text-lg'>projects</h2>
-      <div className='flex gap-6'>
-        <div className='w-64'>
-          <div className='flex h-64 items-center justify-center rounded-2xl border-[0.25px] border-stone-400 border-opacity-40 bg-stone-50'>
-            <Link href={'https://github.com/mkandan/gae-pytube'}>
-              <h2 className='font-sans text-3xl'>dubdubs</h2>
-            </Link>
+            <p className='py-[6px] pl-[10px] text-xs text-stone-400'>
+              2023 &nbsp;with{' '}
+              <Link
+                className='underline'
+                href={'https://twitter.com/eabnelson'}
+              >
+                erik
+              </Link>
+            </p>
           </div>
-          <p className='py-[6px] text-xs text-stone-400'>2023</p>
+
+          {projects
+            .slice()
+            .reverse()
+            .map((project) => (
+              <div>
+                <div
+                  key={project.name}
+                  className='flex h-64 w-64 items-center justify-center rounded-2xl border-[0.25px] border-stone-400 border-opacity-40 bg-stone-50'
+                >
+                  <div className='rounded-md'>
+                    <Link href={project.url}>
+                      <Image
+                        className='w-24'
+                        alt={`${project.name} logo`}
+                        src={project.logo}
+                        width={96}
+                        height={24}
+                      />
+                    </Link>
+                  </div>
+                </div>
+                <p className='py-[6px] pl-[10px] text-xs text-stone-400'>
+                  {project.year}
+                </p>
+              </div>
+            ))}
         </div>
-      </div>
+        <ScrollBar orientation='horizontal' />
+      </ScrollArea>
+
+      <h2 className='pt-12 pb-4 text-lg'>projects</h2>
+      <ScrollArea className='whitespace-nowrap'>
+        <div className='flex w-max space-x-6'>
+          <div>
+            <div className='flex h-64 w-64 items-center justify-center rounded-2xl border-[0.25px] border-stone-400 border-opacity-40 bg-stone-50'>
+              <div className='rounded-md'>
+                <Link href={'https://github.com/mkandan/gae-pytube'}>
+                  <h2 className='font-sans text-3xl'>dubdubs</h2>
+                </Link>
+              </div>
+            </div>
+            <p className='py-[6px] pl-[12px] text-xs text-stone-400'>2023</p>
+          </div>
+        </div>
+        <ScrollBar orientation='horizontal' />
+      </ScrollArea>
+
       <h2 className='py-4 text-lg'>links</h2>
       <div className='tracking-tight text-stone-400'>
         <Link href={'https://twitter.com/man1_kandan'}>twitter</Link>
