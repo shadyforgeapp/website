efc3f1c900a0cdbbfdf5b854fcbd04c3c1985738
Mani Kandan <mani@shadyforge.com>
2024-01-10 12:05:53 -0700

    server github fix?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import {
 import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar'
 import { Info, CalendarDays, GitCommit } from 'lucide-react'
 import { env } from '~/env'
+import { getGithubRepoStats } from '~/server/github'
 
 const projects = [
 	{
@@ -78,70 +79,6 @@ const content = [
 			'https://www.instagram.com/p/Cmvj3tzOOja/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA=='
 	}
 ]
-
-interface Contributor {
-	total: number
-	weeks: [
-		{
-			w: string
-			a: number
-			d: number
-			c: number
-		}
-	]
-	author: {
-		login: string
-		id: number
-		node_id: string
-		avatar_url: string
-		gravatar_id: string
-		url: string
-		html_url: string
-		followers_url: string
-		following_url: string
-		gists_url: string
-		starred_url: string
-		subscriptions_url: string
-		organizations_url: string
-		repos_url: string
-		events_url: string
-		received_events_url: string
-		type: string
-		site_admin: boolean
-	}
-}
-
-export async function getGithubRepoStats(
-	GithubOwner: string,
-	GithubRepo: string,
-	GithubPersonalAccessToken: string
-) {
-	const response = await fetch(
-		`https://api.github.com/repos/${GithubOwner}/${GithubRepo}/stats/contributors`,
-		{
-			headers: {
-				Authorization: `Bearer ${GithubPersonalAccessToken}`,
-				Accept: 'application/vnd.github.v3+json'
-			}
-		}
-	)
-
-	if (response.ok) {
-		const repostats: Contributor[] = (await response.json()) as Contributor[]
-		const targetLogin = 'mkandan'
-
-		const targetAuthor = repostats.find(
-			(item) => item.author.login === targetLogin
-		)
-
-		if (targetAuthor) {
-			const totalCommits = targetAuthor.total
-			return totalCommits
-		} else {
-			return 0
-		}
-	}
-}
 
 export default async function Home() {
 	return (
